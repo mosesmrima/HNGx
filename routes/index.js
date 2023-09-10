e7f5c8440d82510bc9af99bbf80353fcc811f6a2
mrima <mrimamss@gmail.com>
2023-09-10 23:43:04 +0300

    first endpoint

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
+  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+  const currentDate = new Date();
   res.status(200).json({
     slack_name: "Mrima",
-    current_day: "Monday",
-    utc_time: "2023-08-21T15:04:05Z",
+    current_day: daysOfWeek[currentDate.getDay()],
+    utc_time: currentDate.toISOString(),
     track: "backend",
-    github_file_ur: "https://github.com/username/repo/blob/main/file_name.ext",
-    github_repo_url: "https://github.com/username/repo",
+    github_file_ur: "https://github.com/mosesmrima/HNGx/blob/master/routes/index.js",
+    github_repo_url: "https://github.com/mosesmrima/HNGx",
     status_code: 200
 })
 });
